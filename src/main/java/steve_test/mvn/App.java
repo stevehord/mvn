7f44bac5451ee7bbc90182e15c4da6895cf704cc
Stephen Hord <shord@xevos-MacBook-Pro.local>
2017-03-08 14:00:21 -0800

    inital commit

---
+++ src/main/java/steve_test/mvn/App.java
@@ -0,0 +1,16 @@
+package steve_test.mvn;
+
+import org.joda.time.DateTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	DateTime dt = new DateTime();
+        System.out.println( "Hello World!: " + dt.toString("dd:MM:yy") + " Its a great time to be alive3");
+    }
+}
