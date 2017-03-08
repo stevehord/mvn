6a8c64a6fd6cc934ab31913cefe205f380d82fcc
Stephen Hord <shord@xevos-MacBook-Pro.local>
2017-03-08 14:58:35 -0800

    New Version 2.0.2

--- src/main/java/steve_test/mvn/App.java
+++ src/main/java/steve_test/mvn/App.java
@@ -12,5 +12,6 @@ public class App
     {
     	DateTime dt = new DateTime();
         System.out.println( "Hello World!: " + dt.toString("dd:MM:yy") + " Its a great time to be alive3");
+        System.out.println( "This is a new release");
     }
 }
