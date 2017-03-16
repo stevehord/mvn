e3b65a139513d379d3afb6330043fa5b3a067ec8
Stephen Hord <shord@xevo.com>
2017-03-16 11:57:16 -0700

    Hot Signed-off-by: Stephen Hord <shord@xevo.com>

--- src/main/java/steve_test/mvn/App.java
+++ src/main/java/steve_test/mvn/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println( "Hello World!: " + dt.toString("dd:MM:yy") + " Its a great time to be alive3");
         System.out.println( "This is a new release");
         System.out.println( "is a new release");
+        System.out.println( "I have added a Hot Fix");
         
     }
 }
