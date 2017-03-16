6ec4e238fba6a146f9bf84c743e169486d2d0ce4
Stephen Hord <shord@xevo.com>
2017-03-16 11:34:37 -0700

    Build 2.0.11

--- src/main/java/steve_test/mvn/App.java
+++ src/main/java/steve_test/mvn/App.java
@@ -12,7 +12,7 @@ public class App
     {
     	DateTime dt = new DateTime();
         System.out.println( "Hello World!: " + dt.toString("dd:MM:yy") + " Its a great time to be alive3");
-        System.out.println( "is a new release 2.0.10");
+        System.out.println( "is a new release 2.0.11");
         
     }
 }
